2ac083404605f3d271b7bdd891adb8bd4c3dd663
Onur Öztornacı <onur.oztornaci@gmail.com>
2024-05-24 15:12:10 +0100

    parallel.sh

--- parallel.sh
+++ parallel.sh
@@ -10,22 +10,13 @@
 #SBATCH --mem=100G
 #SBATCH --array=1-3
 
+conda init
+conda activate Bismark
 
-WORKFLOW="main.nf"
-CONFIG=$2
-DATA_DIR="/user/work/ag24712/Next_1/" 
-REFERENCE_DIR="/user/work/ag24712/reference/hg38/"  
-RESULTS_DIR="results" 
+source config.sh
 
 DATA_SUBDIR=data$SLURM_ARRAY_TASK_ID
 
-start_time=$(date +"%s")
-
-#conda init ## Required to make conda happy on the nodes
-#source ~/.bashrc ## Required to load what conda init just did
-#conda activate Bismark ## see for more details https://dsbristol.github.io/dst/coursebook/appendix5-bluecrystal.html
-
-## run with -resume to avoid repeating previous analyses
 nextflow run ${WORKFLOW} \
     --reads "${DATA_DIR}/${DATA_SUBDIR}/*_{1,2}.fastq.gz" \
     --t_param 8 \
@@ -34,18 +25,3 @@ nextflow run ${WORKFLOW} \
     --cores 4 \
     --genome_folder ${REFERENCE_DIR} \
     --outdir ${RESULTS_DIR}
-
-cp .nextflow.log ${RESULTS_DIR}/nextflow.log
-
-cd /user/work/ag24712/Next_1/results/results/
-
-bismark2report
-
-bismark2summary
-
-cd ..
-cd ..
-
-multiqc .
-
-
