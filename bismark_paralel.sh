152e170c0cd37a5b76ed15e396230389988a990d
Onur Öztornacı <onur.oztornaci@gmail.com>
2024-06-24 12:08:43 +0100

    Update bismark_paralel.sh

--- bismark_paralel.sh
+++ bismark_paralel.sh
@@ -3,7 +3,7 @@
 nextflow run main.nf \
 -c nextflow.config \
 -resume \
---reads "/user/work/ag24712/fastq_files/*_R{1,2}*.fastq.gz" \
+--reads "" \
 --pipeline bismark \
---genome_folder "/user/work/ag24712/reference/hg19/Bismark/" \
+--genome_folder "" \
 --cores 24
