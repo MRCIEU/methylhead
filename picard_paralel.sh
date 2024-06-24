746bafebab8d8f0938d1c63faae1e74243a7edab
Onur Öztornacı <onur.oztornaci@gmail.com>
2024-06-24 12:08:23 +0100

    Update picard_paralel.sh

--- picard_paralel.sh
+++ picard_paralel.sh
@@ -3,7 +3,8 @@
 nextflow run main.nf \
 -c nextflow.config \
 -resume \
---reads "/user/work/ag24712/fastq_files/*_R{1,2}*.fastq.gz" \
+--reads "" \
 --pipeline picard \
---intervals "/user/work/ag24712/reference/hg19/Twist/covered_targets_Twist_Methylome_hg19_annotated_collapsed_uniq_final" --genome_folder "/user/work/ag24712/reference/hg19/Twist/bwa_index/hg19.fa" \
+--intervals "" \
+--genome_folder "" \
 --cores 24
