4eb891163169e980eab7b1ba9e03682fde375802
Onur Öztornacı <onur.oztornaci@gmail.com>
2024-07-24 20:27:59 +0100

    Update prepare_reference_genome.sh

--- prepare_reference_genome.sh
+++ prepare_reference_genome.sh
@@ -15,28 +15,44 @@ SINGULARITY_IMAGE="dnam_cancer_pipeline_latest.sif"
 # Create the GENOMES directory if it does not exist
 mkdir -p ${GENOMES}
 
-# Download the hg19 reference genome
-wget https://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/hg19.fa.gz -P ${GENOMES}
+# Download the hg19 reference genome if it does not exist
+if [ ! -f ${GENOMES}/hg19.fa ]; then
+    wget https://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/hg19.fa.gz -P ${GENOMES}
+    gunzip ${GENOMES}/hg19.fa.gz 
+fi
 
-# Prepare the genome with bismark using Singularity
-singularity exec ${SINGULARITY_IMAGE} bismark_genome_preparation --path_to_aligner /opt/conda/envs/bismark/bin --verbose ${GENOMES}
+# Index the reference genome using Singularity if index does not exist
+if [ ! -f ${GENOMES}/hg19.fa.fai ]; then
+    singularity exec ${SINGULARITY_IMAGE} samtools faidx ${GENOMES}/hg19.fa
+fi
 
-# Download the BED file
-wget https://www.twistbioscience.com/sites/default/files/resources/2022-06/covered_targets_Twist_Methylome_hg19_annotated_collapsed_final.bed.zip -P ${GENOMES}
+# Prepare the genome with bismark using Singularity if Bismark index does not exist
+if [ ! -d ${GENOMES}/Bisulfite_Genome ]; then
+    singularity exec ${SINGULARITY_IMAGE} bismark_genome_preparation --path_to_aligner /opt/bismark/bismark_genome_preparation --verbose ${GENOMES}
+fi
 
-# Unzip the BED file
-unzip ${GENOMES}/covered_targets_Twist_Methylome_hg19_annotated_collapsed_final.bed.zip -d ${GENOMES}
+# Download the BED file if it does not exist
+if [ ! -f ${GENOMES}/covered_targets_Twist_Methylome_hg19_annotated_collapsed_final.bed ]; then
+    wget https://www.twistbioscience.com/sites/default/files/resources/2022-06/covered_targets_Twist_Methylome_hg19_annotated_collapsed_final.bed.zip -P ${GENOMES}
+    unzip ${GENOMES}/covered_targets_Twist_Methylome_hg19_annotated_collapsed_final.bed.zip -d ${GENOMES}
+fi
 
-# Index the reference genome using Singularity
-singularity exec ${SINGULARITY_IMAGE} samtools faidx ${GENOMES}/hg19.fa
+# Create the sequence dictionary using Singularity if it does not exist
+if [ ! -f ${GENOMES}/hg19.fa.dict ]; then
+    singularity exec ${SINGULARITY_IMAGE} picard CreateSequenceDictionary REFERENCE=${GENOMES}/hg19.fa OUTPUT=${GENOMES}/hg19.fa.dict
+fi
 
-# Create the sequence dictionary using Singularity
-singularity exec ${SINGULARITY_IMAGE} picard CreateSequenceDictionary REFERENCE=${GENOMES}/hg19.fa OUTPUT=${GENOMES}/hg19.fa.dict
+# Convert BED to interval list using Singularity if it does not exist
+if [ ! -f ${GENOMES}/covered_targets_Twist_Methylome_hg19_annotated_collapsed_final.interval_list ]; then
+    singularity exec ${SINGULARITY_IMAGE} picard BedToIntervalList \
+        I=${GENOMES}/covered_targets_Twist_Methylome_hg19_annotated_collapsed_final.bed \
+        O=${GENOMES}/covered_targets_Twist_Methylome_hg19_annotated_collapsed_final.interval_list \
+        SD=${GENOMES}/hg19.fa.dict
+fi
 
-# Convert BED to interval list using Singularity
-singularity exec ${SINGULARITY_IMAGE} picard BedToIntervalList \
-    I=${GENOMES}/covered_targets_Twist_Methylome_hg19_annotated_collapsed_final.bed \
-    O=${GENOMES}/covered_targets_Twist_Methylome_hg19_annotated_collapsed_final \
-    SD=${GENOMES}/hg19.fa.dict
+# Index the reference genome using BWA-MEM with bwameth.py if BWA-MEM index does not exist
+if [ ! -f ${GENOMES}/hg19.fa.bwt ]; then
+    singularity exec ${SINGULARITY_IMAGE} bwameth.py index ${GENOMES}/hg19.fa
+fi
 
-echo "Reference genome preparation completed. Running time is approximately 2 hours."
+echo "Reference genome preparation completed."
