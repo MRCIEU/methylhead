9f6fbd5c4f5d3f4ef634b3a567eb175f62e201af
Onur Öztornacı <onur.oztornaci@gmail.com>
2024-07-25 19:01:44 +0100

    Update prepare_reference_genome.sh

--- prepare_reference_genome.sh
+++ prepare_reference_genome.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# Check if the user provided the GENOMES path
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <path_to_converted_genome_index>"
+# Check if the user provided the GENOMES path and the BED file path
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <path_to_converted_genome_index> <path_to_bed_file>"
     exit 1
 fi
 
 # Define the genome index path from user input
 GENOMES=$1
+
+# Define the BED file path from user input
+BED_FILE=$2
 
 # Singularity image path
 SINGULARITY_IMAGE="dnam_cancer_pipeline_latest.sif"
@@ -26,23 +29,24 @@ if [ ! -f ${GENOMES}/hg19.fa.fai ]; then
     singularity exec ${SINGULARITY_IMAGE} samtools faidx ${GENOMES}/hg19.fa
 fi
 
-# Download the BED file if it does not exist
-if [ ! -f ${GENOMES}/covered_targets_Twist_Methylome_hg19_annotated_collapsed_final.bed ]; then
-    wget https://www.twistbioscience.com/sites/default/files/resources/2022-06/covered_targets_Twist_Methylome_hg19_annotated_collapsed_final.bed.zip -P ${GENOMES}
-    unzip ${GENOMES}/covered_targets_Twist_Methylome_hg19_annotated_collapsed_final.bed.zip -d ${GENOMES}
-fi
-
 # Create the sequence dictionary using Singularity if it does not exist
 if [ ! -f ${GENOMES}/hg19.fa.dict ]; then
     singularity exec ${SINGULARITY_IMAGE} picard CreateSequenceDictionary REFERENCE=${GENOMES}/hg19.fa OUTPUT=${GENOMES}/hg19.fa.dict
 fi
 
 # Convert BED to interval list using Singularity if it does not exist
-if [ ! -f ${GENOMES}/covered_targets_Twist_Methylome_hg19_annotated_collapsed_final.interval_list ]; then
+if [ ! -f ${BED_FILE} ]; then
+    echo "BED file ${BED_FILE} does not exist."
+    exit 1
+fi
+
+INTERVAL_LIST_OUTPUT="${GENOMES}/interval_list"
+
+if [ ! -f ${INTERVAL_LIST_OUTPUT} ]; then
     singularity exec ${SINGULARITY_IMAGE} picard BedToIntervalList \
-        I=${GENOMES}/covered_targets_Twist_Methylome_hg19_annotated_collapsed_uniq_final.bed \
-        O=${GENOMES}/covered_targets_Twist_Methylome_hg19_annotated_collapsed_final.interval_list \
-        SD=${GENOMES}/hg19.fa.dict
+        I=${BED_FILE} \
+        O=${INTERVAL_LIST_OUTPUT} \
+        SD=${GENOMES}/hg19.fa.dict                
 fi
 
 # Prepare the genome with bismark using Singularity if Bismark index does not exist
