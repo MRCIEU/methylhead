67de67394dc3d5ed47aceb62fe2341a47a8aa382
Onur Öztornacı <onur.oztornaci@gmail.com>
2024-08-08 19:39:10 +0100

    Update Full_DNAm_Matrix.R

--- bin/Full_DNAm_Matrix.R
+++ bin/Full_DNAm_Matrix.R
@@ -46,7 +46,7 @@ myobj.filt <- filterByCoverage(myobj,
                                hi.count = NULL,
                                hi.perc = 99.9)
 
-meth <- unite(myobj.filt, destrand = FALSE)
+meth <- unite(myobj.filt, destrand = TRUE)
 pm <- percMethylation(meth)
 pm <- pm / 100
 meth_df <- data.frame(meth)
