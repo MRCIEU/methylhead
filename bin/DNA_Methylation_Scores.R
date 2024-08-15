7ca51cf233f7cc311cdd1221e6f0ed406d2ac906
Onur Öztornacı <onur.oztornaci@gmail.com>
2024-08-16 00:28:38 +0100

    Add files via upload

--- bin/DNA_Methylation_Scores.R
+++ bin/DNA_Methylation_Scores.R
@@ -1,7 +1,18 @@
-#!/usr/bin/R 
 
+library(methylKit)
+library(limma)
+library(IlluminaHumanMethylation450kanno.ilmn12.hg19)
+library(IlluminaHumanMethylation450kmanifest)
+library(RColorBrewer)
+library(stringr)
+library(GenomicRanges)
+library(genomation)
 library(meffonym)
 library(dplyr)
+library(ggplot2)
+library(data.table)
+library(parallel)
+options(mc.cores = 24)
 library(data.table)
 library(parallel)
 options(mc.cores = 24)
@@ -12,8 +23,79 @@ pipeline <- args[1]
 input_dir <- args[2]
 output_dir <- args[3]
 
+
+setwd(input_dir)
+
+
+process_methylation_data <- function(file.vector, sample.ids, pipeline) {
+  if (pipeline == "bismark") {
+    
+    file.list <- as.list(file.vector)
+    myobj <- methRead(file.list,
+                      sample.id = sample.ids,
+                      pipeline = "bismarkCoverage",
+                      assembly = "hg19",
+                      treatment = rep(0, length(sample.ids)),
+                      mincov = 10)
+  } else if (pipeline == "picard") {
+
+    file.list <- as.list(file.vector)
+    myobj <- methRead(file.list,
+                      sample.id = sample.ids,
+                      pipeline = "amp",
+                      assembly = "hg19",
+                      treatment = rep(0, length(sample.ids)),
+                      mincov = 10)
+  } else {
+    stop("Unknown pipeline")
+  }
+  
+  myobj.filt <- filterByCoverage(myobj,
+                                 lo.count = 10,
+                                 lo.perc = NULL,
+                                 hi.count = NULL,
+                                 hi.perc = 99.9)
+  
+  meth <- unite(myobj.filt, destrand = TRUE)
+  pm <- percMethylation(meth)
+  pm <- pm / 100
+  meth_df <- data.frame(meth)
+  ann450k <- getAnnotation(IlluminaHumanMethylation450kanno.ilmn12.hg19)
+  ann450k$loc <- paste(ann450k$chr, ann450k$pos)
+    
+  if (pipeline == "bismark") {
+    meth_df$loc <- paste(meth_df$chr, meth_df$start)
+  } else if (pipeline == "picard") {
+    meth_df$loc <- paste(meth_df$chr, meth_df$end)
+  }  
+  meth_df <- cbind(meth_df, pm)
+  merged_data <- merge(meth_df, ann450k, by = c("loc"))
+  sample.ids2 <- unlist(sample.ids)
+  mm <- merged_data[c("Name", sample.ids2)]
+  methylation <- na.omit(mm)
+  colnames(methylation)[1] <- "CpGs"
+  return(list(methylation = methylation, meth = meth))
+}
+
 if (pipeline == "bismark") {
-twist_meth<-fread(paste0(input_dir ,"/Methylation_matrix.csv"))
+  file.vector <- list.files(pattern = "bismark\\.cov\\.gz", full.names = FALSE)
+  file.vector <- file.vector[!grepl("ctrl", file.vector)]
+  sample.ids <- gsub("_bismark_bt2_pe.deduplicated.bismark.cov.gz", "", basename(file.vector))
+  sample.ids <- as.list(sample.ids)
+} else if (pipeline == "picard") {
+  file.vector <- list.files(pattern = "\\.markdup_CpG\\.methylKit", full.names = FALSE)
+  file.vector <- file.vector[!grepl("ctrl", file.vector)]
+  sample.ids <- gsub(".markdup_CpG.methylKit", "", basename(file.vector))
+  sample.ids <- as.list(sample.ids)
+} else {
+  stop("Unknown pipeline")
+}
+
+result <- process_methylation_data(file.vector, sample.ids, pipeline)
+methylation <- result$methylation
+
+if (pipeline == "bismark") {
+twist_meth<-methylation
 twist_meth<-data.frame(twist_meth)
 rownames(twist_meth)<-twist_meth$CpGs
 names(twist_meth) <- gsub("^X", "", names(twist_meth))
@@ -57,7 +139,7 @@ write.csv(smoking_scores,file = paste0(output_dir,"/Bismark_smoking_scores.csv")
            
 } else if (pipeline == "picard") {
 
-twist_meth<-fread(paste0(input_dir,"/Methylation_matrix.csv"))
+twist_meth<- methylation
 twist_meth<-data.frame(twist_meth)
 rownames(twist_meth)<-twist_meth$CpGs
 names(twist_meth) <- gsub("^X", "", names(twist_meth))
--- bin/Estimate_cell_counts.R
+++ bin/Estimate_cell_counts.R
@@ -1,21 +1,61 @@
 #!/usr/bin/env Rscript
 
+library(methylKit)
 library(dplyr)
 library(data.table)
 library(parallel)
 options(mc.cores = 24)
 
-
 args <- commandArgs(trailingOnly = TRUE)
 pipeline <- args[1]
 data_dir <- args[2]
-dnam_dir <- args[3]
+input_dir <- args[3]
 output_dir <- args[4]
 
 if (pipeline == "bismark") {
-    
-    meth_df <- fread(paste0(dnam_dir,"/DNAm_Full_Matrix.csv"))
-    meth_df <- data.frame(meth_df)
+    file.vector <- list.files(path = input_dir, pattern = "bismark\\.cov\\.gz", full.names = FALSE)
+    file.vector <- file.vector[!grepl("ctrl", file.vector)]
+    sample.ids <- gsub("_bismark_bt2_pe.deduplicated.bismark.cov.gz", "", basename(file.vector))
+    sample.ids <- as.list(sample.ids)
+    file.list <- as.list(file.vector)
+
+    myobj <- methRead(file.list,
+                      sample.id = sample.ids,
+                      pipeline = "bismarkCoverage",                
+                      assembly = "hg19",
+                      treatment = c(rep(0, length(sample.ids))),
+                      mincov = 10)
+} else if (pipeline == "picard") {
+    file.vector <- list.files(path = input_dir, pattern = "\\.markdup_CpG\\.methylKit", full.names = TRUE)
+    file.vector <- file.vector[!grepl("ctrl", file.vector)]
+    sample.ids <- gsub(".markdup_CpG.methylKit", "", basename(file.vector))
+    sample.ids <- as.list(sample.ids)
+    file.list <- as.list(file.vector)
+
+    myobj <- methRead(file.list,
+                      sample.id = sample.ids,
+                      pipeline = "amp",                
+                      assembly = "hg19",
+                      treatment = c(rep(0, length(sample.ids))),
+                      mincov = 10)
+} else {dos
+    stop("Unknown pipeline")
+}
+
+myobj.filt <- filterByCoverage(myobj,
+                               lo.count = 10,
+                               lo.perc = NULL,
+                               hi.count = NULL,
+                               hi.perc = 99.9)
+
+meth <- unite(myobj.filt, destrand = TRUE)
+pm <- percMethylation(meth)
+pm <- pm / 100
+meth_df <- data.frame(meth)
+meth_df <- cbind(meth_df, pm)
+meth_df <- data.frame(meth_df)
+
+if (pipeline == "bismark") {
     
    
     blood_cell_types <- fread(paste0(data_dir,"/blood_cell_types_extended.zip"))
@@ -77,9 +117,7 @@ if (pipeline == "bismark") {
     estimate_cell_counts_Normalized <- estimate_cell_counts / colSums(estimate_cell_counts)
     write.csv(estimate_cell_counts_Normalized, file = paste0(output_dir,"/estimate_cell_counts.csv"))
 } else if (pipeline == "picard") {
-   
-   meth_df <- fread(paste0(dnam_dir,"/DNAm_Full_Matrix.csv"))
-   meth_df <- data.frame(meth_df)
+  
     
     args <- commandArgs(trailingOnly = TRUE)
     
