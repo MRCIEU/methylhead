4de5e2b1b8368f74478d85f6bf543d00aa20d154
Onur Öztornacı <onur.oztornaci@gmail.com>
2024-08-16 20:42:46 +0100

    Update Estimate_cell_counts.R

--- bin/Estimate_cell_counts.R
+++ bin/Estimate_cell_counts.R
@@ -66,7 +66,7 @@ if (pipeline == "bismark") {
     names(meth_df) <- gsub("^X", "", names(meth_df))
     names(meth_df) <- gsub("_R1_001_val_1\\d*", "", names(meth_df))
     
-    sample.ids <- grep("_L001", names(meth_df), value = TRUE)
+    sample.ids<- names(meth_df %>% select(-chr, -start, -end, -strand, -contains("coverage"), -starts_with("num")))
     regions <- blood_cell_types[c("chr","start","end")]
     
     CD4T <- rowMeans(blood_cell_types[c("Blood.T.CD4TT","Blood.T.CD4U7","Blood.T.CD4UM")])
@@ -129,7 +129,7 @@ if (pipeline == "bismark") {
   names(meth_df) <- gsub("^X", "", names(meth_df))
   names(meth_df) <- gsub("_R1_001_val_1\\d*", "", names(meth_df))
 
-sample.ids <- grep("_L001", names(meth_df), value = TRUE)
+sample.ids<- names(meth_df %>% select(-chr, -start, -end, -strand, -contains("coverage"), -starts_with("num")))
 regions<-blood_cell_types[c("chr","start","end")]
 
 CD4T=rowMeans(blood_cell_types[c("Blood.T.CD4TT","Blood.T.CD4U7","Blood.T.CD4UM")])
