9ee31428a572889e51e4055e5dba998fa7b50206
Onur Öztornacı <onur.oztornaci@gmail.com>
2024-09-12 16:28:08 +0100

    Update Full_DNAm_Matrix.R

--- bin/Full_DNAm_Matrix.R
+++ bin/Full_DNAm_Matrix.R
@@ -3,15 +3,11 @@
 library(methylKit)
 library(dplyr)
 library(data.table)
-library(parallel)
-options(mc.cores = 24)
-
 
 args <- commandArgs(trailingOnly = TRUE)
 pipeline <- args[1]
 input_dir <- args[2]
 output_file <- args[3]
-
 
 if (pipeline == "bismark") {
     file.vector <- list.files(path =  input_dir, pattern = "bismark\\.cov\\.gz", full.names = TRUE)
@@ -40,7 +36,6 @@ if (pipeline == "bismark") {
     sample.ids <- gsub(".markdup_CpG.methylKit", "", basename(file.vector))
     sample.ids <- as.list(sample.ids)
     file.list <- as.list(file.vector)
-
     myobj <- methRead(file.list,
                       sample.id = sample.ids,
                       pipeline = "amp",                
@@ -50,13 +45,11 @@ if (pipeline == "bismark") {
 } else {dos
     stop("Unknown pipeline")
 }
-
 myobj.filt <- filterByCoverage(myobj,
                                lo.count = 10,
                                lo.perc = NULL,
                                hi.count = NULL,
                                hi.perc = 99.9)
-
 meth <- unite(myobj.filt, destrand = TRUE,min.per.group=as.integer(length(sample.ids)*0.50))
 pm <- percMethylation(meth)
 pm <- pm / 100
@@ -64,5 +57,3 @@ meth_df <- data.frame(meth)
 meth_df <- cbind(meth_df, pm)
 DNAm_Full_Matrix <- meth_df
 write.csv(DNAm_Full_Matrix, file = output_file, row.names = FALSE)
-
-
