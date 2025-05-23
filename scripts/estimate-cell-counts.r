dce2a91a27860c03d7cde710b732c51ea60972fa
onurslash <onur.oztornaci@gmail.com>
2025-05-23 14:28:11 +0100

    update

--- scripts/estimate-cell-counts.r
+++ scripts/estimate-cell-counts.r
@@ -4,69 +4,69 @@ library(dplyr)
 library(data.table)
 
 args <- commandArgs(trailingOnly = TRUE)
-blood_file <- args[1] ## blood cell type reference data file
-meth_file <- args[2] ## DNA methylation file
-output_file <- args[3] ## cell count estimates output file
 
-meth_df <- data.frame(fread(meth_file))
-   
-blood_cell_types <- fread(blood_file)
-blood_cell_types <- data.frame(blood_cell_types)
+blood_file        <- args[1] ## blood cell type reference data file
+meth_file         <- args[2] ## DNA methylation file
+output_file       <- args[3] ## cell count estimates output file
 
-names(blood_cell_types) <- gsub("GSM\\d+_?", "", names(blood_cell_types))
-names(blood_cell_types) <- gsub(".Z\\d+_?", "", names(blood_cell_types))
-names(meth_df) <- gsub("^X", "", names(meth_df))
-names(meth_df) <- gsub("_R1_001_val_1\\d*", "", names(meth_df))
+meth_df           <- data.frame(fread(meth_file))
+blood_cell_types  <- fread(blood_file)
+blood_cell_types  <- data.frame(blood_cell_types)
 
-sample.ids<- names(meth_df %>% select(-chr, -start, -end))
-regions <- blood_cell_types[c("chr","start","end")]
+names(blood_cell_types) <- gsub("GSM\\d+_?",       "", names(blood_cell_types))
+names(blood_cell_types) <- gsub(".Z\\d+_?",        "", names(blood_cell_types))
+names(meth_df)          <- gsub("^X",              "", names(meth_df))
+names(meth_df)          <- gsub("_R1_001_val_1\\d*", "", names(meth_df)) 
 
-CD4T <- rowMeans(blood_cell_types[c("Blood.T.CD4TT","Blood.T.CD4U7","Blood.T.CD4UM")])
-CD8T <- rowMeans(blood_cell_types[c("Blood.T.CD8TR","Blood.T.CD8U5","Blood.T.CD8UK")])
-NK <- rowMeans(blood_cell_types[c("Blood.NKTM","Blood.NKU1","Blood.NKUF")])
-Mono <- rowMeans(blood_cell_types[c("Blood.MonocytesTP","Blood.MonocytesU3","Blood.MonocytesUH")])
-Bcell <- rowMeans(blood_cell_types[c("Blood.BTX","Blood.BUB","Blood.BUR")])
-Granulocytes <- rowMeans(blood_cell_types[c("Blood.GranulocytesTZ","Blood.GranulocytesUD","Blood.GranulocytesUT")])
-beta.cell.types <- cbind(CD4T, CD8T, NK, Mono, Bcell, Granulocytes)        
+sample.ids <- names(meth_df %>% select(-chr, -start, -end))
+regions    <- blood_cell_types[c("chr","start","end")]
 
-beta <- matrix(ncol=length(sample.ids), nrow=nrow(regions))
-colnames(beta) <- sample.ids
+CD4T        <- rowMeans(blood_cell_types[c("Blood.T.CD4TT",       "Blood.T.CD4U7",       "Blood.T.CD4UM")])
+CD8T        <- rowMeans(blood_cell_types[c("Blood.T.CD8TR",       "Blood.T.CD8U5",       "Blood.T.CD8UK")])
+NK          <- rowMeans(blood_cell_types[c("Blood.NKTM",          "Blood.NKU1",          "Blood.NKUF")])
+Mono        <- rowMeans(blood_cell_types[c("Blood.MonocytesTP",   "Blood.MonocytesU3",   "Blood.MonocytesUH")])
+Bcell       <- rowMeans(blood_cell_types[c("Blood.BTX",           "Blood.BUB",           "Blood.BUR")])
+Granulocytes<- rowMeans(blood_cell_types[c("Blood.GranulocytesTZ","Blood.GranulocytesUD","Blood.GranulocytesUT")])
+beta.cell.types <- cbind(CD4T, CD8T, NK, Mono, Bcell, Granulocytes)
+
+setDT(meth_df)
+meth_dt <- meth_df[, c("chr", "start", "end", sample.ids), with = FALSE]
+setkey(meth_dt, chr, start, end)
+setDT(regions)[, region_id := .I]
+setkey(regions, chr, start, end)
+
+hits <- foverlaps(meth_dt, regions, nomatch = 0L)
+beta_dt <- hits[
+  , lapply(.SD, mean),
+    by = region_id,
+    .SDcols = sample.ids
+]
+beta_dt <- merge(
+  regions[, .(region_id)],
+  beta_dt,
+  by   = "region_id",
+  all.x = TRUE
+)[order(region_id)][, !"region_id"]
+
+beta <- as.matrix(beta_dt)
 colnames(beta) <- gsub("_L001\\d*", "", colnames(beta))
 
-calculate_mean_values <- function(data_frame, chr_value, start_value, end_value) {
-    selected_data <- data_frame %>%
-        filter(chr == chr_value & start >= start_value & end <= end_value)  
-    selected_data <- selected_data[, -c(1:3)]  
-    mean_values <- colMeans(selected_data) 
-    return(mean_values)
-}
-
-meth_df2 <- meth_df[c("chr", "start", "end", unlist(sample.ids))]
-for (i in 1:nrow(regions)) {
-    mean_values <- calculate_mean_values(meth_df2, regions$chr[i], regions$start[i], regions$end[i])
-    beta[i, ] <- mean_values
-}
-
 estimate.cell.counts <- function(beta, beta.cell.types) {
-    stopifnot(nrow(beta) == nrow(beta.cell.types))
-    apply(beta, 2, estimate.cell.counts0, beta.cell.types = beta.cell.types)
+  stopifnot(nrow(beta) == nrow(beta.cell.types))
+  require(quadprog)
+  apply(beta, 2, function(b) {
+    stopifnot(length(b) == nrow(beta.cell.types))
+    loc <- which(!is.na(b) & rowSums(is.na(beta.cell.types)) == 0)
+    bcT.bc <- t(beta.cell.types[loc, ]) %*% beta.cell.types[loc, ]
+    bcT.b  <- t(beta.cell.types[loc, ]) %*% b[loc]
+    diag(bcT.bc) <- diag(bcT.bc) + 1e-8
+    solve.QP(bcT.bc, bcT.b, diag(ncol(beta.cell.types)), rep(0, ncol(beta.cell.types)))$sol -> res
+    names(res) <- colnames(beta.cell.types)
+    res
+  })
 }
 
-estimate.cell.counts0 <- function(beta, beta.cell.types) {
-    stopifnot(length(beta) == nrow(beta.cell.types))
-    require(quadprog)
-    I <- diag(ncol(beta.cell.types))
-    zero <- rep(0, ncol(beta.cell.types))
-    loc.idx <- which(!is.na(beta))
-    bcT.x.bc <- t(beta.cell.types[loc.idx,]) %*% beta.cell.types[loc.idx,]
-    bcT.x.b <- t(beta.cell.types[loc.idx,]) %*% matrix(beta[loc.idx], nrow=length(loc.idx))
-    counts <- solve.QP(bcT.x.bc, bcT.x.b, I, zero)$sol
-    names(counts) <- colnames(beta.cell.types)
-    counts
-}
-
-estimate_cell_counts <- estimate.cell.counts(beta, beta.cell.types)
-estimate_cell_counts_Normalized <- estimate_cell_counts / colSums(estimate_cell_counts)
-colnames(estimate_cell_counts_Normalized) <- gsub("\\.", "-", colnames(estimate_cell_counts_Normalized))
-write.csv(estimate_cell_counts_Normalized, file = output_file)
-
+estimate_cell_counts          <- estimate.cell.counts(beta, beta.cell.types)
+estimate_cell_counts_norm     <- estimate_cell_counts / colSums(estimate_cell_counts)
+colnames(estimate_cell_counts_norm) <- gsub("\\.", "-", colnames(estimate_cell_counts_norm))
+write.csv(estimate_cell_counts_norm, file = output_file)
